d9cdc86348bf222b5ad5162b92d9837c51843399
Your Name <you@example.com>
2021-10-11 18:55:29 +0200

    formating navbar and add items to it

--- src/components/common/HeaderBottom.vue
+++ src/components/common/HeaderBottom.vue
@@ -5,11 +5,11 @@
         <a href="#">All Category</a>
         <ul class="submenu dropdown shadow-lg">
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Mobile & Tablet</a>
-                <ul class="submenu right">
-                  <li><a href="#" style="display:block; line-height:">Lorem, ipsum dolor.</a></li>
-                  <li><a href="#" style="display:block; line-height:">Lorem, ipsum dolor.</a></li>
-                  <li><a href="#" style="display:block; line-height:">Lorem, ipsum dolor.</a></li>
+              <a href="#"><b-icon icon="mobile-alt"></b-icon> Mobile, Tablet</a>
+              <ul class="submenu right">
+                  <li><a href="#" style="display:block; line-height:">Mobiles</a></li>
+                  <li><a href="#" style="display:block; line-height:">Tablet</a></li>
+                  <li><a href="#" style="display:block; line-height:">Mobile And Tablet accessories</a></li>
                   <li><a href="#" style="display:block; line-height:">Lorem, ipsum dolor.</a></li>
                   <li><a href="#" style="display:block; line-height:">Lorem, ipsum dolor.</a></li>
                   <li><a href="#" style="display:block; line-height:">Lorem, ipsum dolor.</a></li>
@@ -17,44 +17,171 @@
               </ul> 
           </li>
           <li>
-              <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+              <a href="#"><b-icon icon="laptop"></b-icon>Computers, Laptops</a>
               <ul class="submenu right">
-                  <li>Lorem, ipsum dolor.</li>
-                  <li>Nesciunt, deleniti unde?</li>
-                  <li>Dignissimos, distinctio molestiae!</li>
+                  <li>Laptop</li>
+                  <li>Computers And monitors</li>
+                  <li>Keyboards And Mouse</li>
+                  <li>Wireless Networks</li>
+                  <li>Drives And Storage</li>
+                  <li>Software And Applications</li>
+                  <li>All Computer Accessories</li>
+                  <li>Printers And Inks</li>
               </ul>
           </li>
           <li>
-              <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+              <a href="#"><b-icon icon="algolia"></b-icon> Home Applianses</a>
               <ul class="submenu right">
-                  <li>Lorem ipsum dolor sit.</li>
-                  <li>Minus soluta amet delectus?</li>
-                  <li>Sunt perferendis temporibus rem.</li>
-                  <li>Repudiandae eum officia voluptas?</li>
-                  <li>Quos distinctio qui sed!</li>
-                  <li>Ratione exercitationem expedita libero.</li>
+                  <li>Washing Machine</li>
+                  <li>Refrigerators</li>
+                  <li>Freezer</li>
+                  <li>Dishwashers</li>
+                  <li>Dryers</li>
+                  <li>Ovens And Stoves</li>
+                  <li>Irons</li>
+                  <li>Vacuum Cleaners</li>
+                  <li>Other Home Appliances</li>
               </ul>
           </li>
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+            <a href="#"><b-icon icon="fan"></b-icon> Air Condition Uint</a>
           </li>
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+              <a href="#"><b-icon icon="tv"></b-icon> TVs, Mointors</a>
+              <ul>
+                  <li>TV's</li>
+                  <li>Home Theaters - Loudspeakers</li>
+                  <li>Projectors</li>
+                  <li>Receivers</li>
+                  <li>Cameras</li>
+                  <li>TV Accessories</li>
+                  <li>All Electronic Accessories</li>
+                  <li>Video Games</li>
+                  <li></li>
+                  <li></li>
+              </ul>
           </li>
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+            <a href="#"><b-icon icon="blender-phone"></b-icon> Kitchen Appliances</a>
+            <ul>
+              <li>Microwave</li>
+              <li>Small Ovens</li>
+              <li>Small Kitchen Appliances</li>
+              <li>Coffee Machines</li>
+              <li>Blenders And Juicers</li>
+              <li>Cooking Utensils</li>
+              <li>All kitchen and dining essentials</li>
+              <li>Kitchen Tools</li>
+              <li>Containers And Storage</li>
+              <li></li>
+            </ul>
           </li>
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Men’s And Women Fashion</a>
+            <ul>
+              <li>Men's Fashion</li>
+              <li>Men's Perfumes</li>
+              <li>Men's Shoes</li>
+              <li>Women's Fashion</li>
+              <li>Women's Perfumes</li>
+              <li>Women's Shoes</li>
+              <li>Make-up</li>
+              <li>Sports Shoes</li>
+              <li>Watches</li>
+              <li>Sunglasses</li>
+              <li>Jewelry</li>
+              <li>Women's Bags And wallets</li>
+              <li>Bags And Travel essentials</li>
+              <li>Fashion Accessories</li>
+              <li></li>
+            </ul>
           </li>
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Sports Equipment</a>
+            <ul>
+              <li>Sports Equipment</li>
+              <li>Sportswear</li>
+              <li>Camping Essentials</li>
+              <li>Hunting Tools</li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
           </li>
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Books</a>
           </li>
           <li>
-            <a href="#"><b-icon icon="credit-card"></b-icon> Lorem ipsum dolor sit.</a>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Home, Furniture And Gadgets</a>
+            <ul>
+              <li>Electrical And Hand Tools</li>
+              <li>Garden Supplies</li>
+              <li>Office Furniture</li>
+              <li>All furnishings</li>
+              <li>Lamps And lighting</li>
+              <li>Home And Furniture Accessories</li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Super Market - Grocery</a>
+            <ul>
+              <li>All Grocery Products</li>
+              <li>Cheese, Dairy And Milk</li>
+              <li>Vegetables And Fruits</li>
+              <li>Coffee, Tea And Drinks</li>
+              <li>Meat And Chicken</li>
+              <li>Waters</li>
+              <li>Cleaning And Washing Products</li>
+              <li>Pet Supplies</li>
+              <li></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Health and Medicine</a>
+            <ul>
+              <li>Body Care Products</li>
+              <li>Oral And Dental Care Products</li>
+              <li>Nutritional Supplements</li>
+              <li>Medicine</li>
+              <li>Sexual Health</li>
+              <li>Medical Equipments</li>
+              <li>Home Care</li>
+              <li></li>
+              <li></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Toys And Kids</a>
+            <ul>
+              <li>Baby Care Essentials</li>
+              <li>Baby Carriages</li>
+              <li>Breastfeeding And Feeding</li>
+              <li>Children's Clothing</li>
+              <li>Baby Toys</li>
+              <li>Bicycles And Buggies</li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#"><b-icon icon="credit-card"></b-icon> Office and school supplies</a>
+            <ul>
+              <li>Back To School</li>
+              <li>School Books</li>
+              <li>Banoaha stationery</li>
+              <li>Drawing Tools</li>
+              <li>School Bags</li>
+              <li>Office Accessories</li>
+              <li>Office Supplies</li>
+              <li></li>
+              <li></li>
+            </ul>
           </li>
         </ul>
       </li>
